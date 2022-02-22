b528e52b100577baa1e0b9c8f959fb6e3896dc24
elviramcqueen <elvira.ohman@student.tabyenskilda.se>
2022-02-22 09:00:48 +0100

    ny

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 // Get the video
 var video = document.getElementById("finresturang");
-var slideIndex = 1;
-showSlides(slideIndex);
 
+var slideshows = document.querySelectorAll('[data-component="slideshow"]');
 
-var slideIndex = [1,1];
-var slideId = ["mySlides1", "mySlides2"]
-showSlides(1, 0);
-showSlides(1, 1);
+// Apply to all slideshows that you define with the markup wrote
+slideshows.forEach(initSlideShow);
 
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
-}
+function initSlideShow(slideshow) {
 
-function showSlides(n, no) {
-  var i;
-  var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  x[slideIndex[no]-1].style.display = "block";
+  var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); // Get an array of slides
+
+  var index = 0, time = 5000;
+  slides[index].classList.add('active');
+
+  setInterval( () => {
+    slides[index].classList.remove('active');
+
+    //Go over each slide incrementing the index
+    index++;
+
+    // If you go over all slides, restart the index to show the first slide and start again
+    if (index === slides.length) index = 0;
+
+    slides[index].classList.add('active');
+
+  }, time);
 }
